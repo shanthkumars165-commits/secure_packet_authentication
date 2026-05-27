5e74dfa2b0becd8c24493b904137a77957d9f427
shanthkumars165-commits <shanthkumars165@gmail.com>
2026-05-27 14:51:00 +0530

    Update tt_um_raksha.v

--- src/tt_um_raksha.v
+++ src/tt_um_raksha.v
@@ -4,42 +4,21 @@ module tt_um_raksha (
     input  wire [7:0] uio_in,
     output wire [7:0] uio_out,
     output wire [7:0] uio_oe,
-
     input  wire ena,
     input  wire clk,
     input  wire rst_n
 );
 
-    parameter SECRET_KEY = 8'hA5;
+    wire auth_ok;
+    wire alert;
 
-    reg auth_ok;
-    reg alert;
+    assign auth_ok = ((ui_in ^ 8'hA5) == 8'hFF);
+    assign alert   = ~auth_ok;
 
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            auth_ok <= 1'b0;
-            alert <= 1'b0;
-        end
-        else begin
-
-            // Packet Authentication Logic
-            if ((ui_in ^ SECRET_KEY) == 8'hFF) begin
-                auth_ok <= 1'b1;
-                alert <= 1'b0;
-            end
-            else begin
-                auth_ok <= 1'b0;
-                alert <= 1'b1;
-            end
-        end
-    end
-
-    // Outputs
     assign uo_out[0] = auth_ok;
     assign uo_out[1] = alert;
     assign uo_out[7:2] = 6'b000000;
 
-    // Unused bidirectional IO
     assign uio_out = 8'b00000000;
     assign uio_oe  = 8'b00000000;
 
